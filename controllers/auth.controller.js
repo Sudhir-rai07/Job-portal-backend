6f5fd9e20d1a07082e741cb267bba81fdda6f0bd
Sudhir Rai <sudhirrai128@gmail.com>
2024-08-30 17:37:39 +0530

    Add login and Logout controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../model/user.model.js";
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
 export const SignUp = async (req, res) => {
   const { username, fullname, email, password } = req.body;
@@ -36,9 +36,62 @@ export const SignUp = async (req, res) => {
       secure: process.env.NODE_ENV !== "development",
     });
 
-    res.status(200).json({success: "User created successfully", token: token});
+    res
+      .status(200)
+      .json({ success: "User created successfully", token: token });
   } catch (error) {
     console.log("Error in SignUp controller ", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const Login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ error: "User not found" });
+
+    const isCorrectPassword = await bcrypt.compare(password, user.password);
+    if (!isCorrectPassword)
+      return res.status(400).json({ error: "Invalid username or password" });
+
+    // setCookieAndGenerateToken
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "15d",
+    });
+
+    res.cookie("token", token, {
+      maxAge: 15 * 24 * 60 * 60 * 1000, //15 days in ms
+      sameSite: "strict",
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+    });
+
+    res.status(200).json({ success: "Logged in", token: token });
+  } catch (error) {
+    console.log('Error in Login controller ', error)
+    res.status(500).json({error: "Internal server error"})
+  }
+};
+
+export const Logout = async (req, res) =>{
+  try {
+    res.cookie("token", "", {
+maxAge: 0
+    })
+    res.status(200).json({success: "Logged out"})
+  } catch (error) {
+    console.log('Error in logout controller ', error)
+    res.status(500).json({error: "Internal server error"})
+  }
+}
+
+
+// export const GetMe = async (req, res) =>{
+//   try {
+  
+//     res.status(200).json()
+//   } catch (error) {
+    
+//   }
+// }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { SignUp } from "../controllers/auth.controller.js";
+import { Login, Logout, SignUp } from "../controllers/auth.controller.js";
 
 const router = Router()
 
 router.post("/signup", SignUp)
+router.post("/login", Login)
+router.post("/logout", Logout)
 
 export default router
