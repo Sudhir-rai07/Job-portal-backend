78fe0b1c5870df4ec25df52465fe4bf6940b7679
Sudhir Rai <sudhirrai128@gmail.com>
2024-08-31 19:07:33 +0530

    Fix typo

--- controllers/job.controller.js
+++ controllers/job.controller.js
@@ -4,7 +4,7 @@ export const PostJob = async (req, res) =>{
     const {title,company,location,employment_type,description,requirements,salary,posted_date,application_deadline } = req.body
     const {userId} = req.user
     try {
-        const newJob = await new Job({
+        const newJob = new Job({
             user:userId,
             title,
             company,
@@ -18,7 +18,7 @@ export const PostJob = async (req, res) =>{
         })
 
         await newJob.save()
-        res.send("Hello")
+        res.status(200).json({message: "Job posted"})
     } catch (error) {
         console.log("Error in post controller ", error)
         res.status(500).json({error: "Internal server error"})
